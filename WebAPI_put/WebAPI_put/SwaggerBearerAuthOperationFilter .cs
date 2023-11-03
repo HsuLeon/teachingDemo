4f2f604c824daf1a3c49e4cabe286aa8769f698e
leon0718 <leon0718@gmail.com>
2023-11-03 12:08:43 +0800

    upload SwaggerBearerAuthOperationFilter.cs

---
+++ WebAPI_put/WebAPI_put/SwaggerBearerAuthOperationFilter .cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WebAPI_put
+{
+    public class SwaggerBearerAuthOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new List<string>()
+                }
+            };
+
+            operation.Security = new List<OpenApiSecurityRequirement> { securityRequirement };
+        }
+    }
+}
